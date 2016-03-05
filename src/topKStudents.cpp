5834826395ee12b76490a1bb152773f360f9d426
mereddyHarshitha <mereddyHarshitha@missionrnd.com>
2016-03-05 19:22:15 +0530

    Submit Commit

--- src/topKStudents.cpp
+++ src/topKStudents.cpp
@@ -21,6 +21,86 @@ struct student {
 	int score;
 };
 
+void merge(struct student *students, int leftIndex, int midIndex, int rightIndex){
+
+	struct student *temp;
+	int i = leftIndex;
+	int j = leftIndex;
+	int k = midIndex + 1;
+	int m;
+	temp = (struct student*)malloc(30 * sizeof(struct student));
+
+	while (j <= midIndex && k <= rightIndex){
+
+		if (students[j].score < students[k].score){
+
+			temp[i].name = students[k].name;
+			temp[i].score = students[k].score;
+			k++;
+
+		}
+		else{
+			temp[i].name = students[j].name;
+			temp[i].score = students[j].score;
+			j++;
+		}
+		i++;
+	}
+	if (j <= midIndex){
+
+		for (m = j; m <= midIndex; m++){
+			temp[i].name = students[m].name;
+			temp[i].score = students[m].score;
+			i++;
+
+		}
+	}
+	else{
+		for (m = k; m <= rightIndex; m++){
+			temp[i].name = students[m].name;
+			temp[i].score = students[m].score;
+			i++;
+
+		}
+	}
+	for (k = leftIndex; k <= rightIndex; k++){
+		students[k].name = temp[k].name;
+		students[k].score = temp[k].score;
+	}
+}
+
+
+void mergeSort(struct student *students, int leftIndex, int rightIndex){
+
+	int midIndex;
+
+	if (leftIndex<rightIndex){
+
+		midIndex = (leftIndex + rightIndex) / 2;
+		mergeSort(students, leftIndex, midIndex);
+		mergeSort(students, midIndex + 1, rightIndex);
+
+		merge(students, leftIndex, midIndex, rightIndex);
+	}
+}
+
 struct student ** topKStudents(struct student *students, int len, int K) {
-	return NULL;
+	
+	struct student **res = (struct student**)calloc(K, sizeof(struct student));
+	int i;
+
+	if (K <= 0)
+		return NULL;
+	else
+		mergeSort(students, 0, len - 1);
+
+	if (K>len){
+		for (i = 0; i < len; i++)
+			res[i] = &students[i];
+	}
+	else{
+		for (i = 0; i < K; i++)
+			res[i] = &students[i];
+	}
+	return res;
 }
